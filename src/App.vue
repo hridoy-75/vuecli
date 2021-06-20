a2d1da37f8ff07098469af073ad29ac2ee0ebaa0
Hridoy75-hub <hcsarker2002@gmail.com>
2021-06-20 21:38:02 +0600

    cli
    vue cli

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+    <comBlog />
+  </div>
+</template>
+<script>
+import comBlog from "./Component/Blog";
+export default {
+  components:{
+    comBlog
+   }
+ }
+ 
+
+</script>
---
+++ src/Component/Blog.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+    <h1>Blog Article</h1>
+    <div v-for="(blog,id) in blogs" :key="id">
+    <h2>{{blog.title}}</h2>
+    <p>{{blog.body}}</p>
+
+       </div>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return{
+            blogs:[]
+
+      }
+    },
+    created(){
+        this.$http.get('https://jsonplaceholder.typicode.com/posts').then((data)=>{
+           
+            this.blogs=data.body.slice(0,10)
+        })
+    }
+  }
+</script>
+<style>
+
+</style>
